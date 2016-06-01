b6649f7fac24387803303195c4078747b7b067fb
JeffRisberg <jrisberg@aol.com>
2016-06-01 15:41:24 -0700

    Updated to match integration work during 5/30/16

--- app/js/app.js
+++ app/js/app.js
@@ -5,28 +5,39 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { routerReducer, routerMiddleware } from 'react-router-redux'
 import thunkMiddleware from 'redux-thunk'
+import createLogger from 'redux-logger';
 import * as storage from 'redux-storage'
 import createEngine from 'redux-storage-engine-localstorage';
 import filter from 'redux-storage-decorator-filter'
 
 import blockStates from './reducers/blockStates';
+import orders from './reducers/orders';
 import categories from './reducers/categories';
 import charities from './reducers/charities';
 import context from './reducers/context';
-import orders from './reducers/orders';
-import pageName from './reducers/pageName';
-import portal from './reducers/portal';
-import selections from './reducers/selections';
 import vendor from './reducers/vendor';
+import portal from './reducers/portal';
+import topCharities from './reducers/topCharities';
+import pageName from './reducers/pageName';
+import selections from './reducers/selections';
+import forms from './reducers/forms';
 
 import AppRoot from './components/AppRoot.js';
 import Page from './components/Page.js';
 
+import {SET_CONTEXT, CLEAR_CONTEXT} from './constants/ActionTypes.js';
+
 var initialContent = {
     blockStates: {},
     categories: null,
-    charities: {idLists: {}, records: {}},
+    charities: {
+        idLists: {},
+        records: {},
+        searchResults: {charityEins: null, pagination: null, loading: false, error: null}
+    },
     context: null,
+    forms: {},
+    topCharities: {idList: [], records: {}, loading: false, error: null},
     orders: {idList: [], records: {}, history: {}},
     pageName: 'Landing',
     portal: null,
@@ -39,6 +50,8 @@ const reducers = combineReducers({
     categories,
     charities,
     context,
+    forms,
+    topCharities,
     orders,
     pageName,
     portal,
@@ -52,10 +65,11 @@ const reducers = combineReducers({
 const storageEngine = filter(createEngine('justgive'), ['context','orders']);
 const storageMiddleware = storage.createMiddleware(storageEngine);
 
+const logger = createLogger();
 const store = createStore(
     reducers,
     initialContent,
-    applyMiddleware(routerMiddleware(hashHistory), thunkMiddleware, storageMiddleware)
+    applyMiddleware(routerMiddleware(hashHistory), thunkMiddleware, logger, storageMiddleware)
 );
 
 const load = storage.createLoader(storageEngine);
@@ -66,12 +80,11 @@ load(store)
                 <Router history={hashHistory}>
                     <Route path="/" component={AppRoot}>
                         <IndexRoute component={Page}/>
+                        <Route path="page/:name" component={Page}/>
                     </Route>
                 </Router>
             </Provider>,
             document.getElementById('app-root')
         );
-
     })
-    //.catch(() => console.log('Failed to load previous state'));
-
+    .catch((error) => console.log('Failed to load previous state: ' + error));
