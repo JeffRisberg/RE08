52054a940d028748e46ba726653e599278104d9a
JeffRisberg <jrisberg@aol.com>
2017-11-25 20:40:51 -0800

    Fetched propTypes from new package for React 15/16

--- app/js/components/Donations.js
+++ app/js/components/Donations.js
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
-
 import { removeDonation } from '../actions/context';
 import { displayUpdateDonation } from '../actions/pageName';
-
-import Donation from './Donation'
+import Donation from './Donation';
 
 /**
  * Shows the donor's current basket contents
@@ -13,55 +12,61 @@ import Donation from './Donation'
  * @since March 2016
  */
 class Donations extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    const donations = this.props.order.donations;
+
+    console.log('this.props.displayButtons: ' + this.props.displayButtons)
+    if (donations != null && donations != undefined && donations.length > 0) {
+      let self = this;
+      const donationNodes = donations.map(function (donation) {
+        return (
+          <div className="row">
+            <div className="col-md-8">
+              <Donation donation={donation} key={donation.id} showCharityAddress={self.props.showCharityAddress}
+                showCharityEin={self.props.showCharityEin}></Donation>
+            </div>
+            {(self.props.displayButtons === "true") ?
+              <div className="col-md-4" style={{ textAlign: 'right' }}>
+
+                <button onClick={ () => {
+                  self.props.displayUpdateDonation(donation)
+                }}>Update Donation
+                </button>
+
+                <button onClick={ () => {
+                  self.props.removeDonation(donation.id)
+                }}>Remove Donation
+                </button>
+              </div>
+              : null}
+          </div>
+        );
+      });
+
+      return (<div>{donationNodes}</div>);
+    } else {
+      return null;
     }
 
-    render() {
-
-        const donations = this.props.order.donations;
-
-        console.log('this.props.displayButtons: ' + this.props.displayButtons)
-        if (donations != null && donations != undefined && donations.length > 0) {
-            let self = this;
-            const donationNodes = donations.map(function (donation) {
-                return (
-                    <div className="row">
-                        <div className="col-md-8">
-                            <Donation donation={donation} key={donation.id} showCharityAddress={self.props.showCharityAddress}
-                                      showCharityEin={self.props.showCharityEin}></Donation>
-                        </div>
-                        {(self.props.displayButtons === "true") ?
-                            <div className="col-md-4" style={{textAlign: 'right'}}>
-
-                                <button onClick={ () => {self.props.displayUpdateDonation(donation)}}>Update Donation</button>
-
-                                <button onClick={ () => {self.props.removeDonation(donation.id)}}>Remove Donation</button>
-                            </div>
-                            : null}
-                    </div>
-                );
-            });
-
-            return (<div>{donationNodes}</div>);
-        } else {
-            return null;
-        }
-
-    }
+  }
 }
 
 Donations.propTypes = {
-    displayButtons: React.PropTypes.bool,
-    showCharityAddress: React.PropTypes.bool,
-    showCharityEin: React.PropTypes.bool
+  displayButtons: PropTypes.bool,
+  showCharityAddress: PropTypes.bool,
+  showCharityEin: PropTypes.bool
 };
 
 const mapStateToProps = (state, ownProps) => {
-    return {};
+  return {};
 };
 
 export default connect(
-    mapStateToProps,
-    {removeDonation, displayUpdateDonation}
+  mapStateToProps,
+  { removeDonation, displayUpdateDonation }
 )(Donations);
