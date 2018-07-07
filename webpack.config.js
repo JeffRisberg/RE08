f33ed79cc7595c01ca2dd652b5fc048f4b25bc9d
JeffRisberg <jrisberg@aol.com>
2018-07-07 10:29:03 -0700

    updated versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
-var webpack = require('webpack');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  mode: 'development',
   entry: {
     javascript: "./app/js/app.js"
   },
@@ -9,12 +11,18 @@ module.exports = {
     filename: "bundle.js"
   },
   module: {
-    loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-      {test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader" }
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [ MiniCssExtractPlugin.loader, "css-loader", "sass-loader" ]
+      },
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.(jpe?g|png|gif|svg)$/i, loader: "file-loader?name=/public/icons/[name].[ext]" }
     ]
   },
   plugins: [
-    new webpack.IgnorePlugin(/^(buffertools)$/) // unwanted "deeper" dependency
+    new webpack.IgnorePlugin(/^(buffertools)$/), // unwanted "deeper" dependency
+    new MiniCssExtractPlugin({ filename: 'public/style.css', allChunks: true })
   ]
 };
